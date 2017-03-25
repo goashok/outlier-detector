8e3f46756f3408ad282d3ba88c0d25a24f4f21a0
Ashok Shamnani <goashok@gmail.com>
2017-03-25 19:26:25 -0400

    Added javadocs

--- src/main/java/org/ashamnani/metrics/EventRateAlgorithm.java
+++ src/main/java/org/ashamnani/metrics/EventRateAlgorithm.java
@@ -4,6 +4,18 @@ package org.ashamnani.metrics;
  * Created by ashok on 3/24/17.
  */
 public enum EventRateAlgorithm {
+    /**
+     * Using this algorithm, the outliers will be detected based on their
+     * actual events in a given second. This can be choppy as event rates for
+     * groups can greatly vary within a second and the detector will constantly mark and swap
+     * OUTLIER(s) based on their to the second event rate event with the slight variations.
+     */
     ACTUAL_RATE_PER_SEC,
+    /**
+     * Using this algorithm, the outliers will be detected based on their exponential
+     * moving average per minute. This will be smoother as their is exponential decay of the
+     * monitored event rate , and the detector will not constantly mark and swap OUTLIER(s) with
+     * small variations. With bigger variations it still will and that is what is needed.
+     */
     EXPONENTIAL_MOVING_AVG_RATE_PER_MIN
 }
